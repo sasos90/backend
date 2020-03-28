52c3b777bb8754e80c13964cd91270e357f57c2f
Saso Sabotin <sasos90@gmail.com>
2020-03-28 19:22:02 +0100

    Test scripts prepared

--- apps/api/src/app/migrations/1570891414095-Initial.ts
+++ apps/api/src/app/migrations/1570891414095-Initial.ts
@@ -1,11 +1,7 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Initial1570891414095 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {}
 
-    public async up(queryRunner: QueryRunner): Promise<any> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<any> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<any> {}
 }
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-docker image build -t backend-api:test -f apps/api/Dockerfile . &&
-docker container run backend-api:test npm run lint &&
-docker container run backend-api:test npm run test
+set -e
+
+docker container rm db_test_service -f || true
+
+docker-compose -f docker-compose.test.yml build --force-rm api &&
+docker-compose -f docker-compose.test.yml up -d db &&
+
+docker-compose -f docker-compose.test.yml run api npm run build api &&
+docker-compose -f docker-compose.test.yml run api npm run lint api &&
+docker-compose -f docker-compose.test.yml run api npm run test api
+
+docker container rm db_test_service -f || true
